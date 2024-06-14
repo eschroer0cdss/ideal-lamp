9034b541b1d5d34f3838b3f44de86cc98e231dde
Eric Schroer CDSS <eric.schroer@dss.ca.gov>
2024-06-14 13:45:03 -0700

    First commit

---
+++ 00_reading_data.R
@@ -0,0 +1,8 @@
+# Loading Directories 
+library(tidyverse)
+library(palmerpenguins)
+
+# Loading Data
+df_penguins_raw <- palmerpenguins::penguins_raw
+
+df_penguins <- palmerpenguins::penguins
---
+++ 01_running_first_script.R
@@ -0,0 +1,3 @@
+# Running '0 Reading Data' Script
+
+source("00_reading_data.R", echo = TRUE)
